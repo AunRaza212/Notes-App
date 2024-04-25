86100040e820f07fd2848c4b8e4cef36fc629780
Aun Raza <aunr76@gmail.com>
2024-04-25 23:43:37 +0500

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import axios from 'axios'
 
-axios.defaults.baseURL='http://localhost:3000'
+axios.defaults.baseURL='https://notes-app-api-taupe.vercel.app/'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
